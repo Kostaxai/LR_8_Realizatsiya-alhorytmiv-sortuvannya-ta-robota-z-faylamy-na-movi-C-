5fb461e1b9fc3ee562f758a6e4984a4555b181c9
Kostaxai <k.v.kyselenko@student.khai.edu>
2026-03-06 11:52:28 +0200

    Delete matrix_utils.h

--- matrix_utils.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-// ----- Функції для роботи з двовимірними масивами (матрицею) -----
-void readMatrix1Based(const std::string& filename, int**& mat, int& M, int& N);
-int findLastBalancedColumn1Based(int** mat, int M, int N);
-void appendResultToMatrixFile(const std::string& filename, int result);
